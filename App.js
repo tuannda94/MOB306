72251f0205d33d798257b0a867beb94400f4f952
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-01-05 09:38:12 +0700

    Create project + App.js

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.text}>Nguyễn Đức Anh Tuấn - PH12345!</Text>
+        <Text style={styles.text}>Địa chỉ: Hà Nội</Text>
+        <Text style={styles.text}>Email: tuannda3@fe.edu.vn</Text>
+        <Text style={styles.text}>SDT: 0392871868</Text>
+        <Text style={styles.text}>Nguyễn Đức Anh Tuấn - PH12345!</Text>
+        <Text style={styles.text}>Địa chỉ: Hà Nội</Text>
+        <Text style={styles.text}>Email: tuannda3@fe.edu.vn</Text>
+        <Text style={styles.text}>SDT: 0392871868</Text>
+        <Text style={styles.text}>Nguyễn Đức Anh Tuấn - PH12345!</Text>
+        <Text style={styles.text}>Địa chỉ: Hà Nội</Text>
+        <Text style={styles.text}>Email: tuannda3@fe.edu.vn</Text>
+        <Text style={styles.text}>SDT: 0392871868</Text>
+        <Text style={styles.text}>Nguyễn Đức Anh Tuấn - PH12345!</Text>
+        <Text style={styles.text}>Địa chỉ: Hà Nội</Text>
+        <Text style={styles.text}>Email: tuannda3@fe.edu.vn</Text>
+        <Text style={styles.text}>SDT: 0392871868</Text>
+        <Text style={styles.text}>Nguyễn Đức Anh Tuấn - PH12345!</Text>
+        <Text style={styles.text}>Địa chỉ: Hà Nội</Text>
+        <Text style={styles.text}>Email: tuannda3@fe.edu.vn</Text>
+        <Text style={styles.text}>SDT: 0392871868</Text>
+        {/* Alt + Shift + Mũi tên xuống */}
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    // nếu dùng sẽ là styles.text
+    color: "red",
+    fontSize: 30,
   },
 });
