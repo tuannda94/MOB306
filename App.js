fb89a72f57d8957b2d6a3941be378ef89c246a08
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-01-05 11:20:20 +0700

    Create project + App.js

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 export default function App() {
+  // Phần viết code logic
+  let name = "Nguyễn Đức Anh Tuấn";
+  let email = "tuannda3@fe.edu.vn";
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.text}>{email}</Text>
+        <Text style={styles.text}>Địa chỉ: Hà Nội</Text>
+        {/* Alt + Shift + Mũi tên xuống: Sao chép dòng */}
+        {/* Ctrl + D: Bôi những phần nội dung tương tự */}
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  text: {
+    color: "red",
+    fontSize: 30,
+    fontStyle: "italic",
+    fontWeight: "bold",
   },
 });
