2166614f0d136698e4642e6c36060563f2eb0c99
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-01-06 09:40:15 +0700

    Create project + App.js

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  // Phần code logic:
+  let name = "NDAT";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Đưa giá trị biến vào trong cặp ngoặc nhọn {} */}
+      <Text style={styles.text}> {name} - PH12345</Text>
+      <Text style={styles.text}>Email: tuannda3@fe.edu.vn</Text>
+      <Text style={styles.text}>SĐT: 0392871868</Text>
+      {/* Alt + Shift + Mũi tên xuống: để sao chép dòng */}
+      {/* Ctrl + D: Bôi vào những phần nội dung giống nhau */}
+      {/* Ctrl + X: Nếu không bôi văn bản thì sẽ xoá cả dòng */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  text: {
+    // tên của phần thay đổi giao diện
+    color: "red",
+    fontStyle: "italic",
+    fontWeight: "bold",
+    fontSize: 30,
   },
 });
