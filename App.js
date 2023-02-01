e4675bc2976305f458249565811c0b35a9048a00
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-02-01 10:47:08 +0700

    Modal, Add

--- App.js
+++ App.js
@@ -1,121 +1,79 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Modal,
+  FlatList,
+  TextInput,
+  Button,
+  Pressable
+} from 'react-native';
 import {useState} from 'react';
-import ProductName,
-  { ProductDesc as PD } // as chỉ áp dụng với component k default
-  from "./src/screens/ProductName";
-import ProductList from "./src/screens/ProductList";
-// import { ProductDesc } from "./src/screens/ProductName";
 
-export default function App() {
-  // const [tên_state, tên_pt_cập_nhật_gt] = useState(gt mặc định)
-  const [countState, setCountState] = useState(0);
-  const [status, setStatus] = useState(false);
+const App = () => {
+  const [isShowForm, setShowForm] = useState(false);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [list, setList] = useState([]);
 
-  // Phần xử lý logic
-  let className = "CP17301123123123123";
-  let username = "tuannda3";
-  let age = 20;
+  const onSave = () => {
+    // 1. Cau truc du lieu obj moi de luu
+    const newItem = {
+      id: list[list.length - 1]?.id + 1 || 1,
+      name: name,
+      price: price
+    };
+    // 2. Dinh nghia mang du lieu moi roi luu
+    const newList = [...list, newItem];
+    setList(newList);
+    // 3. Dong modal va dua TextInput ve gia tri mac dinh
+    setShowForm(false); setName(''); setPrice(0);
+  };
 
-  if (age > 20) {
-    username = 'tuannda3 - Người lớn';
-  } else {
-    username = 'tuannda3 - Trẻ em';
-  }
-
-  function showData (label = '', value = '') {
-    return label + ': ' + value;
-  }
-  // arrow function
-  const arrowShowData = (label, value) => {
-    return label + ': ' + value;
-  }
-  const arrowMiniShowData = (label, value) => (label + ': ' + value);
-  // Tình huống tăng giá trị count khi bấm nút
-  let count = 0;
-  const plus = () => count++;
-
-  // Ds truyền sang cho ProductList
-  const list = [
-    {id: 1, name: 'Bún bò huế', price: 50000},
-    {id: 2, name: 'Bún trộn', price: 40000},
-    {id: 3, name: 'Phở trộn', price: 35000},
-    {id: 4, name: 'Phở trộn', price: 35000},
-    {id: 5, name: 'Phở trộn', price: 35000},
-    {id: 6, name: 'Phở trộn', price: 35000},
-  ];
+  const onDelete = (itemId) => {
+    const newList = list.filter((item) => {
+      return item.id !== itemId;
+    });
+    setList(newList);
+  };
 
   return (
-    // <ScrollView>
-      <View style={styles.container}>
-        <ProductList data={list} />
-
-{/*
-        <ProductName name={'Cơm niêu'}    />
-        <PD desc={'Mô tả cơm niêu'} />
-        <ProductName
-          name={'Cơm rang'}
+    <View>
+      {isShowForm ? null : <Button title='Them moi'
+        onPress={() => setShowForm(true)}
+      />}
+      <Modal visible={isShowForm} animationType="slide">
+        <View>
+          <Text>{name} - {price}</Text>
+          <TextInput placeholder='Ten SP'
+            // value={name}
+            onChangeText={(text) => setName(text)}
+          />
+          <TextInput placeholder='Gia SP' keyboardType='numeric'
+            onChangeText={(text) => setPrice(text)}
+          />
+          <Button title='Huy'
+            onPress={() => setShowForm(false)}
+          />
+          <Button title='Luu' onPress={() => onSave()} />
+        </View>
+      </Modal>
+      <View>
+        <FlatList
+          data={list}
+          renderItem={({item}) => <View>
+            <Text>{item.id}</Text>
+            <Text>{item.name}</Text>
+            <Text>{item.price}</Text>
+            <Pressable onPress={() => onDelete(item.id)}>
+              <Text>Xoa</Text>
+            </Pressable>
+          </View>}
+          keyExtractor={(item) => item.id}
         />
-        <ProductName
-          name={'Hủ tiếu'}
-          description={'Mô tả dùng trong PN Hủ tiếu'}
-        />
-
-        <Button
-          // title={
-          //   status
-          //     ? 'Bấm để ẩn giao diện'
-          //     : 'Bấm để hiện giao diện'
-          // }
-          title={`Bấm để ${status ? 'ẩn' : 'hiện'} giao diện`}
-          onPress={() => setStatus(!status)}
-        />
-        {
-          status
-            ? <>
-                <Text style={styles.text}>Count: {count}</Text>
-                <Button
-                  onPress={() => plus()}
-                  title={'Bấm để tăng count'}
-                  // onPress={plus}
-                />
-              </>
-            : <Text>Không chỉnh sửa được</Text>
-        }
-
-
-        <Text style={styles.text}>CS: {countState}</Text>
-        <Button
-          title={'Bấm để tăng count'}
-          onPress={() => setCountState(countState + 1)}
-          // onPress={plus}
-        />
-
-        <Text style={styles.text}>{className}_RN!</Text>
-        <Text style={styles.text}>
-          {arrowShowData('Email', 'tuannda3@fe.edu.vn')}
-        </Text>
-        <Text style={styles.text}>
-          {arrowMiniShowData('Địa chỉ', 'Hà Nội')}
-        </Text> */}
-
-        {/* Alt + Shift + Mũi tên xuống: Sao chép dòng */}
-        {/* Ctrl + D: Bôi phần nội dung giống nhau */}
-        {/* <StatusBar style="auto" /> */}
       </View>
-    // </ScrollView>
+    </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "red",
-    fontSize: 40,
-  },
-});
+};
+const styles = StyleSheet.create({});
+export default App;
