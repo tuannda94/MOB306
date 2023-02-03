b52425f6846403fa0fb41ff47e89da4d14627a3c
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-02-03 10:21:53 +0700

    Update, Delete

--- App.js
+++ App.js
@@ -14,9 +14,28 @@ const App = () => {
   const [isShowForm, setShowForm] = useState(false);
   const [name, setName] = useState('');
   const [price, setPrice] = useState(0);
+  const [editId, setEditId] = useState(0);
   const [list, setList] = useState([]);
 
+  const onClose = () => {
+    setShowForm(false); setName(''); setPrice(0);
+    setEditId(0);
+  }
+
   const onSave = () => {
+    // 0. Nếu có editId != 0 thì là đang chỉnh sửa
+    if (editId != 0) {
+      const newEditList = list.map(item => {
+        if (item.id == editId) {
+          // Gán gt mới = gt đã lưu ở state sau khi nhập
+          item.name = name;
+          item.price = price;
+        }
+        return item;
+      });
+      setList(newEditList);
+      return onClose();
+    }
     // 1. Cau truc du lieu obj moi de luu
     const newItem = {
       id: list[list.length - 1]?.id + 1 || 1,
@@ -27,7 +46,7 @@ const App = () => {
     const newList = [...list, newItem];
     setList(newList);
     // 3. Dong modal va dua TextInput ve gia tri mac dinh
-    setShowForm(false); setName(''); setPrice(0);
+    return onClose();
   };
 
   const onDelete = (itemId) => {
@@ -35,6 +54,17 @@ const App = () => {
       return item.id !== itemId;
     });
     setList(newList);
+  };
+
+  const onEdit = (editId) => {
+    // 1. Mở modal ra
+    setShowForm(true);
+    // 2. Tìm kiếm phần tử có id = editId
+    const editItem = list.find(item => item.id == editId);
+    // 3. Hiển thị dữ liệu của phần tử vừa tìm được
+    setName(editItem.name);
+    setPrice(editItem.price);
+    setEditId(editId); // setEditId(editItem.id);
   };
 
   return (
@@ -46,14 +76,15 @@ const App = () => {
         <View>
           <Text>{name} - {price}</Text>
           <TextInput placeholder='Ten SP'
-            // value={name}
+            value={name}
             onChangeText={(text) => setName(text)}
           />
           <TextInput placeholder='Gia SP' keyboardType='numeric'
+            value={price}
             onChangeText={(text) => setPrice(text)}
           />
           <Button title='Huy'
-            onPress={() => setShowForm(false)}
+            onPress={() => onClose()}
           />
           <Button title='Luu' onPress={() => onSave()} />
         </View>
@@ -65,6 +96,9 @@ const App = () => {
             <Text>{item.id}</Text>
             <Text>{item.name}</Text>
             <Text>{item.price}</Text>
+            <Pressable onPress={() => onEdit(item.id)}>
+              <Text>Sua</Text>
+            </Pressable>
             <Pressable onPress={() => onDelete(item.id)}>
               <Text>Xoa</Text>
             </Pressable>
