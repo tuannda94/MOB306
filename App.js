ce55e0c858965c4d8b71db66241cdcec284e80df
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-02-03 08:09:36 +0700

    Delete, Update

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import {
   Button,
   TextInput,
   Modal,
-  FlatList
+  FlatList,
+  Pressable
 } from "react-native";
 import {useState} from 'react';
 
@@ -26,14 +27,35 @@ export default function App() {
   // De danh sach render lai khi co du lieu moi thi can 1 ds dang state
   const [productList, setProductList] = useState(data);
   const [isShowAdd, setShowAdd] = useState(false);
+  const [editId, setEditId] = useState(0);
   const [nameValue, setNameValue] = useState('');
   const [priceValue, setPriceValue] = useState(0);
 
+  const handleClose = () => {
+    setNameValue(''); setPriceValue(0); setEditId(0);
+    setShowAdd(false);
+  }
+
   const handleAdd = () => {
+    // Nếu có editId thì là đang sửa và cần cập nhật pt
+    if (editId) {
+      const newEditProductList = [...productList];
+      for (let i = 0; i < newEditProductList.length; i++) {
+        if (newEditProductList[i].id == editId) {
+          newEditProductList[i].name = nameValue;
+          newEditProductList[i].price = priceValue;
+        }
+      }
+      setProductList(newEditProductList);
+      return handleClose();
+    }
+
     // Khi bam Luu se goi ham nay
     // 1. Dinh nghia object moi se duoc them vao mang du lieu
     const newItem = {
-      id: productList[productList.length - 1].id + 1,
+      id: productList.length == 0
+        ? 1
+        : productList[productList.length - 1].id + 1,
       name: nameValue,
       price: priceValue
     };
@@ -43,7 +65,25 @@ export default function App() {
     // 3. Cap nhat ds moi de hien thi
     setProductList(newProductList);
     // 4. Cap nhat input ve ds mac dinh va dong modal
-    setNameValue(''); setPriceValue(0); setShowAdd(false);
+    return handleClose();
+  };
+
+  const handleDelete = (deleteId) => {
+    const newProductList = productList
+      .filter(item => item.id !== deleteId);
+    setProductList(newProductList);
+  };
+
+  // Hàm Sửa Chạy khi bấm nút sửa ở từng phần tử
+  const handleEdit = (editId) => {
+    // 1. Hiển thị modal lên
+    setShowAdd(true);
+    // 2. Truyền giá trị cần sửa vào TextInput
+    const editItem = productList
+      .find(item => item.id == editId);
+    setNameValue(editItem.name);
+    setPriceValue(editItem.price);
+    setEditId(editItem.id);
   };
 
   return (
@@ -65,15 +105,22 @@ export default function App() {
             value={priceValue}
             onChangeText={(text) => setPriceValue(text)}
           />
-          <Button title="Huy" onPress={() => setShowAdd(false)} />
+          <Button title="Huy" onPress={() => handleClose()} />
           <Button title="Luu" onPress={() => handleAdd()}/>
         </View>
       </Modal>
       <FlatList
         data={productList}
         renderItem={({item}) => <View>
+          <Text>{item.id}</Text>
           <Text>Ten SP: {item.name}</Text>
           <Text>Gia SP: {item.price} VND</Text>
+          <Pressable onPress={() => handleEdit(item.id)}>
+            <Text>Sua</Text>
+          </Pressable>
+          <Pressable onPress={() => handleDelete(item.id)}>
+            <Text>Xoa</Text>
+          </Pressable>
         </View>}
         keyExtractor={(item) => item.id}
       />
