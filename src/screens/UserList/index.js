0c58f198ed11c221d5c8698e99ff5b804320297b
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-02-16 09:17:40 +0700

    API fetch

--- src/screens/UserList/index.js
+++ src/screens/UserList/index.js
@@ -1,32 +1,40 @@
 import {View, Text, FlatList} from 'react-native';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 // Yêu cầu: Định nghĩa 1 mảng gồm 3 phần tử
 // Mỗi phần tử gồm: id, name, age
 // Sử dụng FlatList để hiển thị
-
+// json-server db.json --watch
 const UserList = (props) => {
     const navigation = props.navigation;
+    const [list, setList] = useState([]);
+    const [isLoading, setLoading] = useState(true);
 
-    const data = [
-        {id: 1, name: 'Nguyen Van A', age: 20},
-        {id: 2, name: 'Nguyen Van B', age: 30},
-        {id: 3, name: 'Nguyen Van C', age: 40},
-    ];
-    const [list, setList] = useState(data);
+    useEffect(() => {
+        // Khi component vừa render xong thì sẽ call API lấy dữ liệu về
+        fetch('http://10.0.2.2:3000/users')
+            .then((res) => res.json()) // khi api call thành công thì chạy vào then
+            // kết quả của lần then trước là res.json() là tham số data của then tiếp theo
+            .then((data) => {setList(data); setLoading(false);})
+            .catch((err) => console.log(err)) // khi api call thất bại thì chạy vào catch
+    }, []);
 
     return (
         <View>
             <Text>Trang danh sách</Text>
-            <FlatList
-                data={list}
-                renderItem={({item}) => <View>
-                    <Text>ID: {item.id}</Text>
-                    <Text>Tên: {item.name}</Text>
-                    <Text>Tuổi: {item.age}</Text>
-                </View>}
-                keyExtractor={(item) => item.id}
-            />
+            {isLoading
+                ? <Text style={{fontSize: 50}}>Loading...</Text>
+                : <FlatList
+                    data={list}
+                    renderItem={({item}) => <View>
+                        <Text>ID: {item.id}</Text>
+                        <Text>Tên: {item.name}</Text>
+                        <Text>Tuổi: {item.age}</Text>
+                    </View>}
+                    keyExtractor={(item, index) => index}
+                />
+            }
+
         </View>
     );
 };
